adddf47a9bfc8d7ef11c06de59be68b00c848dd0
beatrizdaddea <beatrizchith@gmail.com>
2024-07-21 20:38:22 -0300

    feat: resize image

--- yolov8-web-app/yolo_app.py
+++ yolov8-web-app/yolo_app.py
@@ -3,8 +3,8 @@ import numpy as np
 import base64
 import cv2
 import os
-
 from ultralytics import YOLO
+
 model = YOLO('best.pt')
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
@@ -15,13 +15,16 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 def predict_on_image(image_stream):
-    image = cv2.imdecode( np.asarray(bytearray(image_stream.read()), dtype=np.uint8) , cv2.IMREAD_COLOR)
-
+    image = cv2.imdecode(np.asarray(bytearray(image_stream.read()), dtype=np.uint8), cv2.IMREAD_COLOR)
+    
+    # Redimensionar a imagem para 640x640
+    image = cv2.resize(image, (640, 640))
+    
     results = model.predict(image, classes=0, conf=0.5)
     for i, r in enumerate(results):
         im_bgr = r.plot(conf=True)
-
-    return im_bgr
+    
+    return im_bgr, image
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -35,14 +38,15 @@ def home():
             return render_template('index.html', error='No selected file')
 
         if file and allowed_file(file.filename):
+            predicted_image, resized_image = predict_on_image(file.stream)
 
-            predicted_image = predict_on_image(file.stream)
-
+            # Encode the predicted image to base64
             retval, buffer = cv2.imencode('.png', predicted_image)
             detection_img_base64 = base64.b64encode(buffer).decode('utf-8')
 
-            file.stream.seek(0)
-            original_img_base64 = base64.b64encode(file.stream.read()).decode('utf-8')
+            # Encode the resized original image to base64
+            retval, buffer = cv2.imencode('.png', resized_image)
+            original_img_base64 = base64.b64encode(buffer).decode('utf-8')
 
             return render_template('result.html', original_img_data=original_img_base64, detection_img_data=detection_img_base64)
 
@@ -51,5 +55,3 @@ def home():
 if __name__ == '__main__':
     os.environ.setdefault('FLASK_ENV', 'development')
     app.run(debug=False, port=5000, host='0.0.0.0')
-
-
